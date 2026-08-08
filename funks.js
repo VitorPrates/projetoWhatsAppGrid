9c5f2d9121ffabedaf5af544ae7c2d91588c339e
VitorPrates <vitoraaprates@gmail.com>
2026-08-07 21:36:04 -0300

    tentando resolver a exibição de mensagens

--- funks.js
+++ funks.js
@@ -3,10 +3,10 @@ const display_conversa = document.querySelector(".display_conversa")
 function enviar_mensagem(form)
 {
     let msg = new FormData(form)
-    console.log(msg.get("msg_field"));
+    // console.log(msg.get("msg_field"));
     let texto_enviar = document.createElement("p")
-    texto_enviar.innerHTML=msg.get("msg_field")
     texto_enviar.classList.add("enviando")
+    texto_enviar.innerHTML += msg.get("msg_field")
     display_conversa.appendChild(texto_enviar)
     form.reset()
 }
