508bfd3ddc7afabaec370b300d57882995cf256d
VitorPrates <vitoraaprates@gmail.com>
2026-08-12 18:55:54 -0300

    correções no envio de mensagens

--- funks.js
+++ funks.js
@@ -5,15 +5,24 @@ const view_perfil = document.querySelector(".perfil")
 
 
 
+let form_envio_msg = document.getElementById("form_envio_msg")
 
+form_envio_msg.addEventListener("submit", (e) => {
+    e.preventDefault()
+    enviar_mensagem(form_envio_msg)
+})
 function enviar_mensagem(form)
 {
     let msg = new FormData(form)
     // console.log(msg.get("msg_field"));
+    if (msg.get("msg_field") == "" || msg.get("msg_field").trim() == "") {
+        return
+    }
     let texto_enviar = document.createElement("p")
     texto_enviar.classList.add("enviando")
     texto_enviar.innerHTML += msg.get("msg_field")
     display_conversa.appendChild(texto_enviar)
+    display_conversa.scrollTo(0,display_conversa.scrollHeight)
     form.reset()
 }
 
